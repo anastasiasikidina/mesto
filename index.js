958eab5b048920e9ec29d889691ef3e30c6456a8
Anastasia Sikidina <anastasia.sikidina@yandex.ru>
2021-02-07 12:11:33 +0300

    refactor: переработан код

--- index.js
+++ index.js
@@ -1,123 +1,35 @@
-const overlays = document.querySelectorAll('.page__overlay');
-const overlayLook = document.querySelector('.page__overlay_type_look');
-const overlayAdd = document.querySelector('.page__overlay_type_add');
-const overlayEdit = document.querySelector('.page__overlay_type_edit');
-const buttonEdit = document.querySelector('.profile__button_edit');
-const nameInput = overlayEdit.querySelector('.popup__input_el_name');
-const jobInput = overlayEdit.querySelector('.popup__input_el_about');
-const name = document.querySelector('.profile__name');
-const job = document.querySelector('.profile__about');
-const formEdit = document.querySelector('.popup_do_edit');
-const formAdd = document.querySelector('.popup_do_add');
-const buttonAdd = document.querySelector('.profile__button_add');
-const template = document.querySelector('.template');
-const contentElements = document.querySelector('.content__elements');
-const captionInput = overlayAdd.querySelector('.popup__input_el_caption');
-const imageInput = overlayAdd.querySelector('.popup__input_el_image');
-const captionPopup = document.querySelector('.popup__caption');
-const imagePopup = document.querySelector('.popup__image');
-const formElement = document.querySelector('.popup_type_form');
+const root = document.querySelector('.root');
+const popup = root.querySelector('.popup');
+const editButton = root.querySelector('.profile__edit-button');
+const closeButton = root.querySelector('.popup__close-button');
+const saveButton = root.querySelector('.popup__save-button');
+const formElement = popup.querySelector('.popup__form');
+let nameInput = formElement.querySelector('input[name=name]');
+let jobInput = formElement.querySelector('input[name=profession]')
+let jobField = root.querySelector('.profile__profession');
+let nameField = root.querySelector('.profile__name');
 
-function openPopup (popup) {
-  clearForm(popup);
-  popup.classList.add('page__overlay_active');
-};
+const openPopup = (e) => {
+    e.preventDefault();
+    nameInput.value = nameField.textContent;
+    jobInput.value = jobField.textContent;
+    popup.classList.toggle("popup_opened");
 
-function closePopup (popup) {
-  popup.classList.remove('page__overlay_active');
-};
+}
 
-function handleFormSubmitAdd (evt) {
-  const myObject = {
-    name: captionInput.value,
-    link: imageInput.value};
-  getCard(myObject);
-  captionInput.value = '';
-  imageInput.value = '';
-  renderCardPrepend(myObject);
-  closePopup(overlayAdd);
-};
+const closePopup = (e) => {
+    e.preventDefault();
+    popup.classList.toggle("popup_opened");
+}
 
-function handleFormSubmitEdit (evt) {
-  name.textContent = nameInput.value;
-  job.textContent = jobInput.value;
-  closePopup(overlayEdit);
-};
+const submitHandler = (e) => {
+    e.preventDefault();
+    nameField.textContent = nameInput.value;
+    jobField.textContent = jobInput.value;
+    popup.classList.toggle("popup_opened");
 
-function handleOverlaysItem (overlay) {
-  overlay.addEventListener('click', handleOverlay);
-};
+}
 
-function handleOverlay (event) {
-  if (event.target === event.currentTarget) { 
-    closePopup(event.target);
-  };
-};
-
-function getCard (el) {
-  const htmlElement = template.content.cloneNode(true);
-  htmlElement.querySelector('.elements__caption').innerText = el.name;
-  htmlElement.querySelector('.elements__image').setAttribute('src', el.link);
-  htmlElement.querySelector('.elements__image').setAttribute('alt', el.name);
-  htmlElement.querySelector('.elements__button-delete').addEventListener('click', handleDelete);
-  htmlElement.querySelector('.elements__button-like').addEventListener('click', handleLike);
-  htmlElement.querySelector('.elements__image').addEventListener('click', () => handleLook(el));
-  return htmlElement;
-};
-
-function renderCardAppend (card) {
-  contentElements.append(getCard(card));
-};
-
-function renderCardPrepend (card) {
-  contentElements.prepend(getCard(card));
-};
-
-function handleDelete(evt) {
-  evt.target.closest('.elements__item').remove();
-};
-
-function handleLike(evt) {
-  evt.target.classList.toggle('elements__button-like_active');
-};
-
-function handleLook(el) {
-  imagePopup.setAttribute('src', el.link);
-  imagePopup.setAttribute('alt', `Фото ${el.name}`);
-  captionPopup.textContent = el.name;
-  overlayLook.querySelector('.popup__button').addEventListener('click', () => closePopup(overlayLook));
-  openPopup(overlayLook);
-};
-
-function clearForm (overlay) { 
-  const inputList = Array.from(overlay.querySelectorAll('.popup__input')); 
-  const formElement = overlay.querySelector('.popup');
-  const buttonElement = overlay.querySelector('.popup__submit');
-  const object = {
-    inputErrorClass: 'popup__input_type_error',
-    inputErrorClassActive: 'popup__input-error_active'
-};
-  inputList.forEach ((inputItem) => {
-    inputItem.value = '';
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
-  hideInputError(formElement, inputItem, object);
-  buttonElement.classList.add('popup__submit_inactive');
-  formElement.removeEventListener('submit', handleFormSubmit);
-  formElement.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-  });
-  });
-};
-
-overlays.forEach (popup => {
-  popup.querySelector('.popup__button').addEventListener('click', () => closePopup(popup));
-  handleOverlaysItem(popup);
-});
-
-initialCards.forEach(card => {
-  renderCardAppend(card);
-});
-
-buttonAdd.addEventListener('click', () => openPopup (overlayAdd));
-buttonEdit.addEventListener('click', () => openPopup (overlayEdit));
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", submitHandler);
