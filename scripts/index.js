0574ad18f0f18243187ac11862910951f2bd5c39
Anastasia Sikidina <anastasia.sikidina@yandex.ru>
2021-02-08 12:22:37 +0300

    fix: исправления по 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
-const root = document.querySelector('.root');
-const popup = root.querySelector('.popup');
-const editButton = root.querySelector('.profile__edit-button');
-const closeButton = root.querySelector('.popup__close-button');
-const saveButton = root.querySelector('.popup__save-button');
-const formElement = popup.querySelector('.popup__form');
+let root = document.querySelector('.root');
+let popup = root.querySelector('.popup');
+let editButton = root.querySelector('.profile__edit-button');
+let closeButton = root.querySelector('.popup__close-button');
+let formElement = popup.querySelector('.popup__form');
 let nameInput = formElement.querySelector('input[name=name]');
 let jobInput = formElement.querySelector('input[name=profession]')
 let jobField = root.querySelector('.profile__profession');
 let nameField = root.querySelector('.profile__name');
 
-const openPopup = (e) => {
+let openPopup = (e) => {
     e.preventDefault();
     nameInput.value = nameField.textContent;
     jobInput.value = jobField.textContent;
@@ -17,16 +16,17 @@ const openPopup = (e) => {
 
 }
 
-const closePopup = (e) => {
+/*const closePopup = (e) => {
     e.preventDefault();
-    popup.classList.toggle("popup_opened");
-}
+    popup.classList.toggle("popup_opened"); тут я немного запуталась - нужно совсем убрать это из кода(все что закомитела или толлько последнюю функцию с toggle)?
+
+}*/
 
 const submitHandler = (e) => {
     e.preventDefault();
     nameField.textContent = nameInput.value;
     jobField.textContent = jobInput.value;
-    popup.classList.toggle("popup_opened");
+    popup.classList.toggle("popup_opened");/*тут уже тоже непонятки какую функцию надо вызвать :( */
 
 }
 
